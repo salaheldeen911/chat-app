6ea338dc03a2bdfda79b99150e32b628308cf682
salah <salah.eldeen.mail@gmail.com>
2022-09-21 23:30:17 +0200

    updated

--- src/stores/AuthStore.js
+++ src/stores/AuthStore.js
@@ -4,15 +4,46 @@ export const AuthStore = defineStore("auth", {
   state() {
     return {
       user: null,
-      token: "",
+      encryptedToken: "",
       status: false,
     };
   },
   actions: {
     setData(user, token) {
       this.user = user;
-      this.token = token;
+      this.encryptedToken = this.crypt("hell", token);
       this.status = true;
+    },
+
+    crypt(salt, text) {
+      const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+      const byteHex = (n) => ("0" + Number(n).toString(16)).substr(-2);
+      const applySaltToChar = (code) =>
+        textToChars(salt).reduce((a, b) => a ^ b, code);
+
+      return text
+        .split("")
+        .map(textToChars)
+        .map(applySaltToChar)
+        .map(byteHex)
+        .join("");
+    },
+
+    decrypt(salt, encoded) {
+      const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+      const applySaltToChar = (code) =>
+        textToChars(salt).reduce((a, b) => a ^ b, code);
+      return encoded
+        .match(/.{1,2}/g)
+        .map((hex) => parseInt(hex, 16))
+        .map(applySaltToChar)
+        .map((charCode) => String.fromCharCode(charCode))
+        .join("");
+    },
+  },
+  getters: {
+    token(state) {
+      if (state.encryptedToken) this.decrypt("hell", state.encryptedToken);
     },
   },
 
@@ -21,7 +52,7 @@ export const AuthStore = defineStore("auth", {
       {
         key: "user",
         storage: localStorage,
-        paths: ["status", "token", "user"],
+        paths: ["status", "encryptedToken", "user"],
       },
     ],
     enabled: true,
